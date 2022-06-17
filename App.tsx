5dcc341acc0b9609a5e41900a5946f37226a6260
tuananh294 <tuananh2941994@gmail.com>
2022-06-17 08:51:42 +0700

    first commit

---
+++ App.tsx
@@ -0,0 +1,180 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * Generated with the TypeScript template
+ * https://github.com/react-native-community/react-native-template-typescript
+ *
+ * @format
+ */
+
+import React from 'react';
+import {
+  SafeAreaView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  ImageBackground,
+  View,
+  Dimensions,
+  Button,
+  FlatList,
+  TouchableWithoutFeedback
+} from 'react-native';
+import { SketchCanvas, SketchCanvasRef } from 'rn-perfect-sketch-canvas';
+import api from './api';
+
+const App = () => {
+  const canvasRef = React.useRef<SketchCanvasRef>(null);
+  const image = { uri: "http://10.10.10.184:9090/static/mobile/sample/output.png" };
+  const { width, height } = Dimensions.get("window");
+
+  const words = [
+    'し',
+    'ま',
+    'ん',
+    'ろ',
+    'よ',
+    'こ',
+    'へ',
+    'は',
+    'た',
+    'も',
+    'け'
+  ]
+  const [wordId, setWordId] = React.useState(0);
+  const [result, setResult] = React.useState('');
+
+  const CustomPicker = ({ label, data, currentIndex, onSelected }) => {
+    return (
+      <>
+        <Text style={styles.title}>{label}</Text>
+        <View style={styles.wrapperHorizontal}>
+          <FlatList
+            bounces
+            showsVerticalScrollIndicator={false}
+            showsHorizontalScrollIndicator={false}
+            horizontal
+            data={data}
+            keyExtractor={(item, idx) => String(item)}
+            renderItem={({ item, index }) => {
+              const selected = index === currentIndex;
+              return (
+                <TouchableWithoutFeedback onPress={() => onSelected(index)}>
+                  <View
+                    style={[
+                      styles.itemStyleHorizontal,
+                      selected && styles.itemSelectedStyleHorizontal
+                    ]}
+                  >
+                    <Text
+                      style={{
+                        textAlign: "center",
+                        color: selected ? "black" : "grey",
+                        fontWeight: selected ? "bold" : "normal"
+                      }}
+                    >
+                      {item + ""}
+                    </Text>
+                  </View>
+                </TouchableWithoutFeedback>
+              );
+            }}
+          />
+        </View>
+      </>
+    );
+  }
+
+  const onCheck = async () => {
+    const res = await api.compareStroke(JSON.stringify(canvasRef.current?.toPoints()), width)
+    setResult(res.mess)
+  };
+  return (
+    <SafeAreaView>
+      <ImageBackground
+        key={(new Date()).getTime()}
+        source={{ uri: "http://10.10.10.184:9090/static/mobile/sample/output.png" + '?time' + (new Date()).getTime(), headers: { Pragma: 'no-cache' } }}
+        resizeMode="cover"
+        style={{ width: width, height: width }}>
+        <SketchCanvas
+          ref={canvasRef}
+          strokeColor={'black'}
+          strokeWidth={15}
+          containerStyle={{ width: width, height: width }}
+        />
+      </ImageBackground>
+      <Button
+        onPress={() => canvasRef.current?.reset()}
+        title="Reset"
+        color="blue"
+      />
+      <Button
+        onPress={() => {
+          onCheck()
+        }}
+        title="Check"
+        color="blue"
+      />
+      <CustomPicker
+        label="Select Word"
+        data={words}
+        currentIndex={wordId}
+        onSelected={(val) => {
+          api.getSample(words[val])
+          canvasRef.current?.reset()
+          setWordId(val)
+          setResult('')
+        }}
+      />
+      <Text>{result}</Text>
+    </SafeAreaView>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    paddingTop: StatusBar.currentHeight,
+    backgroundColor: "#ecf0f1",
+    padding: 8
+  },
+  paragraph: {
+    paddingVertical: 0,
+    textDecorationColor: "yellow",
+    margin: 24
+  },
+  wrapperHorizontal: {
+    height: 54,
+    justifyContent: "center",
+    color: "black",
+    marginBottom: 12
+  },
+  itemStyleHorizontal: {
+    marginRight: 10,
+    height: 50,
+    padding: 8,
+    borderWidth: 1,
+    borderColor: "grey",
+    borderRadius: 25,
+    textAlign: "center",
+    justifyContent: "center"
+  },
+  itemSelectedStyleHorizontal: {
+    borderWidth: 2,
+    borderColor: "#DAA520"
+  },
+  platformContainer: {
+    marginTop: 8,
+    borderTopWidth: 1
+  },
+  platformContainerTitle: {
+    marginTop: 8
+  },
+  title: {
+    fontWeight: "bold",
+    marginVertical: 4
+  }
+});
+
+export default App;
---
+++ api.ts
@@ -0,0 +1,109 @@
+import axios, { AxiosRequestConfig, Method } from 'axios';
+import qs from 'querystring';
+
+const METHOD_GET = 'get';
+const METHOD_POST = 'post';
+const METHOD_PUT = 'put';
+const METHOD_DELETE = 'delete';
+
+type DataRequest = {
+  url: string;
+  method?: Method;
+  headers?: any;
+  params?: any;
+  isJSON?: boolean;
+  isLoading?: boolean;
+  isLoadingInSide?: boolean;
+  isMessage?: boolean;
+  isCallbackMessage?: boolean;
+  isEnableLog?: boolean;
+  id?: string;
+};
+
+class API {
+  private message: string = '';
+  async request(data: DataRequest) {
+    const { method, url, id, headers = {}, params = {}, isJSON = false, isLoading = true, isMessage = true, isLoadingInSide = true, isEnableLog = false, isCallbackMessage = false } = data;
+    let _params = params;
+    // console.log('API > request > _id: ', _id);
+
+    if (isJSON) {
+      headers['Content-Type'] = 'application/json';
+    }
+
+    if (isJSON && (method === METHOD_POST || method === METHOD_PUT)) {
+      headers['Content-Type'] = 'application/json';
+    } else if (method === METHOD_POST || method === METHOD_PUT) {
+      headers['Content-Type'] = 'application/x-www-form-urlencoded';
+      _params = qs.stringify(params);
+    }
+
+    const config: AxiosRequestConfig = { url, headers, method, validateStatus: () => true, timeout: 15000 };
+    if (method === METHOD_GET) {
+      config.params = _params;
+    } else {
+      config.data = _params;
+    }
+    // console.log('API > request > config: ', config);
+
+    const startTime = new Date().getTime();
+    const res = await axios(config);
+
+    const endTime = new Date().getTime();
+    if (url !== this.getUrl('/master-data/list-coins') && isEnableLog) {
+      console.log('-----------------------------');
+      console.log(`[${method?.toUpperCase()}] [${endTime - startTime}ms] [${url}] [${JSON.stringify(_params)}]\n${JSON.stringify(res)}`);
+      console.log('-----------------------------');
+    }
+    if (isCallbackMessage) {
+      return res;
+    } else {
+      if (res.status === 200 || res.status === 201) {
+        return res?.data;
+      } else {
+        if (res?.data?.message && isMessage) {
+        }
+        return null;
+      }
+    }
+  }
+
+  async get(data: DataRequest) {
+    data.method = METHOD_GET;
+    return await this.request(data);
+  }
+
+  async post(data: DataRequest) {
+    data.method = METHOD_POST;
+    return await this.request(data);
+  }
+
+  async put(data: DataRequest) {
+    data.method = METHOD_PUT;
+    return await this.request(data);
+  }
+
+  async delete(data: DataRequest) {
+    data.method = METHOD_DELETE;
+    return await this.request(data);
+  }
+
+  getUrl(suffix?: string) {
+    // console.log('API > getUrl > network: ', network);
+    // return URL.default.api(network) + suffix;
+    return suffix ? "http://10.10.10.184:9090/" + suffix : "http://10.10.10.184:9090/";
+  }
+
+  async getSample(word: string) {
+    const url = this.getUrl('get_sample');
+    const params = { text_input: word };
+    return await this.post({ url, params });
+  }
+
+  async compareStroke(point: string, size: number) {
+    const url = this.getUrl('compare_stroke');
+    const params = { strokes: point, size: size };
+    return await this.post({ url, params });
+  }
+}
+export default new API();
